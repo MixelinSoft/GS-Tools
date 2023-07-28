802280d1a8066722ce2f9a5e6d998484dedccf3f
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2023-07-28 08:34:04 +0300

    Fix bug in backcalibrate, directory

--- tools/backcalibrate/backcalibrate.js
+++ tools/backcalibrate/backcalibrate.js
@@ -1,3 +1,97 @@
+<<<<<<< HEAD
+const selectGSWindow = document.querySelector(".selectGS-window"),
+  selectGSForm = document.querySelector(".selectGS-form"),
+  calibrateWindow = document.querySelector(".calibrate-window"),
+  calibrateForm = document.querySelector("#calibrateForm"),
+  alertZone = document.querySelector("#calibate__alert"),
+  resultZone = document.querySelector("#calibrate__result");
+
+// let gsId;
+
+function selectGS() {
+  for (key in gsDB) {
+    const option = document.querySelector("#gsID");
+    if (!(gsDB[key].tables == undefined)) {
+      const gs = `<option value="${key}">АЗС№ ${gsDB[key].gsId} "${gsDB[key].gsFirm}"</option>`;
+      option.insertAdjacentHTML("beforeend", gs);
+    }
+  }
+  selectGSForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const gsId = document.querySelector("#gsID").value;
+    if (gsId === "unSelect") {
+      alertZone.innerHTML = `Не выбрана АЗС!`;
+      resultZone.scrollIntoView();
+    } else {
+      createFuelTypesList(gsId);
+      windowSwitcher(selectGSWindow, calibrateWindow);
+      alertZone.innerHTML = "";
+    }
+  });
+}
+
+function createFuelTypesList(gsId) {
+  for (key in gsDB[gsId].tables) {
+    const option = document.querySelector("#autocalibrateFuelType"),
+      tables = gsDB[gsId].tables,
+      fuel = `<option value="${key}">${tables[key].tankId}. ${tables[key].type} </option>`;
+
+    option.insertAdjacentHTML("beforeend", fuel);
+  }
+  selectFuelType(gsId);
+}
+
+function selectFuelType(gsId) {
+  calibrateForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let fuelType = document.querySelector("#autocalibrateFuelType").value;
+    let value = +document
+      .querySelector("#calibrateValue")
+      .value.replace(",", ".");
+
+    if (fuelType === "unSelect") {
+      alertZone.innerHTML = `Не выбран вид топлива!`;
+      resultZone.scrollIntoView();
+    } else {
+      alertZone.innerHTML = "";
+      backCalibrate(fuelType, value, gsId);
+    }
+  });
+}
+
+function backCalibrate(type, value, gsId) {
+  resultZone.innerHTML = "";
+
+  const table = gsDB[gsId].tables[type].capacityTable,
+    tube = gsDB[gsId].tables[type].tube,
+    valueWihoutTube = value - tube;
+
+  if (table.includes(valueWihoutTube)) {
+    const index = table.indexOf(valueWihoutTube);
+    resultZone.innerHTML = `Предполагаемая высота топлива - ${index} см`;
+  } else {
+    table.forEach((element, index) => {
+      if (valueWihoutTube > element && valueWihoutTube < table[index + 1]) {
+        let tail = Math.round(
+          (valueWihoutTube - element) / ((table[index + 1] - element) / 10)
+        );
+
+        resultZone.innerHTML = `Предполагаемая высота топлива - ${index}.${tail} см`;
+        resultZone.scrollIntoView();
+      }
+    });
+  }
+}
+
+function windowSwitcher(windowHide, windowShow) {
+  windowHide.classList.add("display-none");
+  windowShow.classList.remove("display-none");
+}
+
+selectGS();
+=======
 const selectGSWindow = document.querySelector(".selectGS-window"),
   selectGSForm = document.querySelector(".selectGS-form"),
   calibrateWindow = document.querySelector(".calibrate-window"),
@@ -97,3 +191,4 @@ backBtn.addEventListener("click", (e) => {
   windowSwitcher(calibrateWindow, selectGSWindow);
   document.querySelector("#calibrateValue").value = "";
 });
+>>>>>>> fd371ef8cf53870925947b3a48afd8bd583027d6
--- tools/directory/directory.js
+++ tools/directory/directory.js
@@ -1,3 +1,53 @@
+<<<<<<< HEAD
+const cardSection = document.querySelector(".gs-card__section"),
+  cardTitle = document.querySelector("#gsCardTitle"),
+  cardFirm = document.querySelector("#gsCardFirm"),
+  cardPhone = document.querySelector("#gsCardNumber"),
+  cardAddress = document.querySelector("#gsCardAddress"),
+  cardMap = document.getElementById("gsCardMap"),
+  cardMapContainer = document.querySelector(".gs-card__map"),
+  alertZone = document.querySelector("#calibate__alert"),
+  backBtn = document.querySelector("#backJS");
+
+for (key in gsDB) {
+  const option = document.querySelector("#gsID");
+  if (!(gsDB[key].phone == undefined)) {
+    const gs = `<option value="${key}">АЗС№ ${gsDB[key].gsId} "${gsDB[key].gsFirm}"</option>`;
+    option.insertAdjacentHTML("beforeend", gs);
+  }
+}
+
+document.querySelector(".form").addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+document.querySelector("#gsID").addEventListener("change", () => {
+  const option = document.querySelector("#gsID").value;
+  alertZone.innerHTML = "";
+  if (option === "unSelect") {
+    alertZone.innerHTML = `Не выбрана АЗС!`;
+  } else {
+    const gs = gsDB[option];
+    cardTitle.innerHTML = `АЗС №${gs.gsId}`;
+    cardFirm.innerHTML = `ТОВ "${gs.gsFirm}"`;
+    cardPhone.href = `tel:${gs.phone}`;
+    cardPhone.innerHTML = `${gs.phone.substr(0, 3)} (${gs.phone.substr(
+      3,
+      3
+    )}) ${gs.phone.substr(6, 3)}-${gs.phone.substr(9, 2)}-${gs.phone.substr(
+      11,
+      2
+    )}`;
+    cardAddress.innerHTML = `${gs.address}`;
+    if (gs.gps !== undefined) {
+      cardMapContainer.classList.remove("display-none");
+      console.log(gs.gps);
+      cardMap.src = gs.gps;
+    }
+    cardSection.classList.remove("display-none");
+  }
+});
+=======
 const confirmBtn = document.querySelector("gsSelectConfirm"),
   selectSection = document.querySelector(".gs-select__section"),
   cardSection = document.querySelector(".gs-card__section"),
@@ -48,3 +98,4 @@ document.querySelector("#gsSelectConfirm").addEventListener("click", () => {
     cardSection.classList.remove("display-none");
   }
 });
+>>>>>>> fd371ef8cf53870925947b3a48afd8bd583027d6
